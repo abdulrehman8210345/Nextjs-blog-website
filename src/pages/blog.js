b72de943e792a323bd3d286bf26c247da1ef5eaa
abdulrehman8210345 <abdulrehman8210345@gmail.com>
2024-02-10 00:49:27 +0500

    project completed

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import styles from "@/styles/blog.module.css";
 import Link from 'next/link';
-import * as fs from 'fs';
+// import * as fs from 'fs';
 
 
 const Blog = (props) => {
