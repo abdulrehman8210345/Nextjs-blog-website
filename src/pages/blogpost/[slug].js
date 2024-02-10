369297a889fe1efd8d1ed935b59f2f08b93d98e3
abdulrehman8210345 <abdulrehman8210345@gmail.com>
2024-02-10 09:42:53 +0500

    static webpage generated

--- src/pages/blogpost/[slug].js
+++ src/pages/blogpost/[slug].js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { useRouter } from 'next/router';
 import styles from "@/styles/blogpost.module.css";
-// import * as fs from 'fs';
+import * as fs from 'fs';
 
 const Slug = (props) => {
     //The below 2 lines are use to find query by using useRouter
@@ -25,33 +25,33 @@ const Slug = (props) => {
 
 export default Slug;
 
-// export async function getStaticPaths() {
-//   return {
-//       paths: [
-//         { params: { slug: 'learn-js' } },
-//         { params: { slug: 'learn-mongodb' } },
-//         { params: { slug: 'learn-nodejs' } },
+export async function getStaticPaths() {
+  return {
+      paths: [
+        { params: { slug: 'learn-js' } },
+        { params: { slug: 'learn-mongodb' } },
+        { params: { slug: 'learn-nodejs' } },
         
-//       ],
-//       fallback: true // false or 'blocking'
-//   };
-// }
+      ],
+      fallback: true // false or 'blocking'
+  };
+}
 
 
 
-// export async function getStaticProps(context) {
-//   const {slug} = context.params;
-//   let data=await fs.promises.readFile(`blogdata/${slug}.json`,'utf-8');
-//   let filecontent = await JSON.parse(data);
-//     // console.log(req.query.slug);
-//     // console.log(data)
-//     // res.status(200).json(JSON.parse(data));
+export async function getStaticProps(context) {
+  const {slug} = context.params;
+  let data=await fs.promises.readFile(`blogdata/${slug}.json`,'utf-8');
+  let filecontent = await JSON.parse(data);
+    // console.log(req.query.slug);
+    // console.log(data)
+    // res.status(200).json(JSON.parse(data));
 
   
-//   return {
-//     props: { filecontent }, // will be passed to the page component as props
-//   }
-// }
+  return {
+    props: { filecontent }, // will be passed to the page component as props
+  }
+}
 
 //Static side generation of slugs(quesries), here we have three slugs (quesries) possibilities thats why we use getstaicpaths() function for genearting number of slugs and and getstaicprops for fetching data at export/build time, once it is exported then no changes updation takes place it (out) directory : out: is our static site having html files
 
@@ -60,15 +60,15 @@ export default Slug;
 
 //This below function is used for SSR, as explained in blog.js
 //this function runs on a server side before the page rendered
-export async function getServerSideProps(context) {
+// export async function getServerSideProps(context) {
 
-  // console.log(context.query);
-  const {slug}=context.query;
-   let data=await fetch(`http://localhost:3000/api/getblog?slug=${slug}`);
-   let getblog=await data.json();
+//   // console.log(context.query);
+//   const {slug}=context.query;
+//    let data=await fetch(`http://localhost:3000/api/getblog?slug=${slug}`);
+//    let getblog=await data.json();
 
   
-  return {
-    props: { getblog }, // will be passed to the page component as props
-  }
- }
+//   return {
+//     props: { getblog }, // will be passed to the page component as props
+//   }
+//  }
